5291506c710f5875da311ef736d0c7cfecf45449
Antonio Rhee <antoniolrhee@gmail.com>
2016-12-22 16:09:07 -0800

    after thus work on groupchats

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,8 +1,10 @@
 var Message = require('../models/message');
+var Groupchat = require('../models/groupchat');
 
 module.exports = {
   index: index,
   create: create,
+  show: show
 }
 
 function index(req, res, next) {
@@ -13,8 +15,18 @@ function index(req, res, next) {
   });
 }
 
+function show(req, res, next) {
+  Message.findById(id, function(err, message) {
+    if (err) throw err;
+
+    res.json(message);
+  });
+}
+
 function create(req, res, next) {
   var newMessage = new Message(req.body);
+  console.log(req.params)
+  // newMessage.chat =
   newMessage.save(function(err, savedMessage) {
     if (err) next (err);
 
